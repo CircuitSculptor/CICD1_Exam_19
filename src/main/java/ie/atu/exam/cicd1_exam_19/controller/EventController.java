de522081a7abf25ac8befd0c1de065fabc72e203
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2025-11-19 14:15:50 +0000

    added started code to controller

--- src/main/java/ie/atu/exam/cicd1_exam_19/controller/EventController.java
+++ src/main/java/ie/atu/exam/cicd1_exam_19/controller/EventController.java
@@ -1,4 +1,18 @@
 package ie.atu.exam.cicd1_exam_19.controller;
 
+import ie.atu.exam.cicd1_exam_19.service.EventRegistrationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class EventController {
+    private final EventRegistrationService service;
+    public EventController(EventRegistrationService service) {
+        this.service = service;
+    }
+
 }
