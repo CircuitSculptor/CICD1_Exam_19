31e368601dd5022a1e63943d78dfe6ee38aad1dd
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2025-11-19 14:25:02 +0000

    Added 4 variables to eventreg

--- src/main/java/ie/atu/exam/cicd1_exam_19/model/EventRegistration.java
+++ src/main/java/ie/atu/exam/cicd1_exam_19/model/EventRegistration.java
@@ -1,4 +1,29 @@
 package ie.atu.exam.cicd1_exam_19.model;
 
+import ie.atu.exam.cicd1_exam_19.service.EventRegistrationService;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventRegistration {
+
+    @NotBlank
+    private String attendeeName;
+
+    @Email
+    private String attendeeEmail;
+
+    @Pattern(regexp = "tk-[0-9]{4}")
+    private String ticketCode;
+
+    @Positive
+    private int quantity;
+
 }
